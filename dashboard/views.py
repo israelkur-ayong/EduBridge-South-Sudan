f628ba8f15577e1faad59d7443ac5c33d8945a8d
Israel Kur <i.ayong@alustudent.com>
2026-08-16 20:21:02 +0200

    Add profile picture uploads

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 
 @login_required
 def dashboard(request):
-    return render(request, "dashboard/dashboard.html")
+    profile = request.user.profile
+
+    return render(
+        request,
+        "dashboard/dashboard.html",
+        {
+            "profile": profile,
+        }
+    )
